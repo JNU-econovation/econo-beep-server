a2fe2ddc48d9407d3a00e10e07744cd7bbf56114
Kwon770 <sckwon770@gmail.com>
2022-05-25 14:04:05 +0900

    init

---
+++ src/main/java/com/econo/econobeepserver/EconoBeepServerApplication.java
@@ -0,0 +1,13 @@
+package com.econo.econobeepserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconoBeepServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconoBeepServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/econo/econobeepserver/EconoBeepServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.econo.econobeepserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconoBeepServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
