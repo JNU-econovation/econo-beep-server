f94876bf2917309c12a55662a167e6bae69980ed
Kwon770 <sckwon770@gmail.com>
2023-05-30 23:38:52 +0900

    feat: Add UserFakerIdp class to return dummy account info

--- src/main/java/com/econo/econobeepserver/service/User/IdpConfig.java
+++ src/main/java/com/econo/econobeepserver/service/User/IdpConfig.java
@@ -1,11 +1,13 @@
 package com.econo.econobeepserver.service.User;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class IdpConfig {
 
     public UserIdp userIdp() {
+//        return new UserFakerIdp();
         return new UserEconoIdp();
     }
 }
---
+++ src/main/java/com/econo/econobeepserver/service/User/UserFakerIdp.java
@@ -0,0 +1,27 @@
+package com.econo.econobeepserver.service.User;
+
+import com.econo.econobeepserver.dto.User.UserIdpIdDto;
+import com.econo.econobeepserver.dto.User.UserIdpTokenDto;
+
+public class UserFakerIdp implements UserIdp {
+
+    @Override
+    public UserIdpIdDto getUserIdpIdDtoByIdpId(Long idpId) {
+        return UserIdpIdDto.builder()
+                .id(99L)
+                .year(21)
+                .username("권순찬")
+                .email("sckwon770@gmail.com")
+                .build();
+    }
+
+    @Override
+    public UserIdpTokenDto getUserIdpTokenDtoByIdpToken(String idpToken) {
+        return UserIdpTokenDto.builder()
+                .id(99L)
+                .year(21)
+                .username("권순찬")
+                .email("sckwon770@gmail.com")
+                .build();
+    }
+}
